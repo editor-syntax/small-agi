56ee473572245324fcc03871ffe2ba0e926a64e1
– sanz <sanzbernward@gmail.com>
2023-04-16 20:28:22 +0800

    Update index.js
    Fixes #1

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
-const { default: axios } = require('axios');
-const { default: Pinecone } = require('@pinecone-io/client');
-const { default: prompt } = require('prompt-sync')();
-const { default: chalk } = require('chalk');
+require("dotenv").config();
+const axios = require("axios");
+const Pinecone = require("@pinecone-io/client");
+const prompt = require("prompt-sync")();
+const chalk = require("chalk");
+const { spawn } = require("child_process");
+const fs = require("fs");
+const path = require("path");
 
 // Engine configuration
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-const OPENAI_API_MODEL = process.env.OPENAI_API_MODEL || 'gpt-3.5-turbo';
+const OPENAI_API_MODEL = process.env.OPENAI_API_MODEL || "gpt-3.5-turbo";
 const OPENAI_TEMPERATURE = parseFloat(process.env.OPENAI_TEMPERATURE) || 0.0;
 
 const PINECONE_API_KEY = process.env.PINECONE_API_KEY;
@@ -20,19 +23,19 @@ const OBJECTIVE = process.env.OBJECTIVE;
 const INITIAL_TASK = process.env.INITIAL_TASK || process.env.FIRST_TASK;
 
 // Check if we know what we are doing
-if (!OBJECTIVE) throw new Error('OBJECTIVE environment variable is missing from .env');
-if (!INITIAL_TASK) throw new Error('INITIAL_TASK environment variable is missing from .env');
+if (!OBJECTIVE) throw new Error("OBJECTIVE environment variable is missing from .env");
+if (!INITIAL_TASK) throw new Error("INITIAL_TASK environment variable is missing from .env");
 
-if (OPENAI_API_MODEL.toLowerCase().includes('gpt-4')) {
-  console.log(chalk.red.bold('\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****\n'));
+if (OPENAI_API_MODEL.toLowerCase().includes("gpt-4")) {
+  console.log(chalk.red.bold("\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****\n"));
 }
 
 // Configure OpenAI and Pinecone
 const openai = axios.create({
-  baseURL: 'https://api.openai.com/v1',
+  baseURL: "https://api.openai.com/v1",
   headers: {
     Authorization: `Bearer ${OPENAI_API_KEY}`,
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
@@ -41,18 +44,12 @@ const pinecone = new Pinecone({
   environment: PINECONE_ENVIRONMENT,
 });
 
-// Create Pinecone index
-const dimension = 1536;
-const metric = 'cosine';
-const podType = 'p1';
-
 (async () => {
   if (!(await pinecone.listIndexes()).includes(YOUR_TABLE_NAME)) {
-    await pinecone.createIndex(YOUR_TABLE_NAME, { dimension, metric, podType });
+    await pinecone.createIndex(YOUR_TABLE_NAME, { dimension: 1536, metric: "cosine", podType: "p1" });
   }
 })();
 
-// Task list
 const taskList = [];
 
 function addTask(task) {
@@ -60,9 +57,9 @@ function addTask(task) {
 }
 
 async function getAdaEmbedding(text) {
-  const response = await openai.post('/embeddings', {
-    input: text.replace(/\n/g, ' '),
-    model: 'text-embedding-ada-002',
+  const response = await openai.post("/embeddings", {
+    input: text.replace(/\n/g, " "),
+    model: "text-embedding-ada-002",
   });
   return response.data.data[0].embedding;
 }
@@ -70,13 +67,27 @@ async function getAdaEmbedding(text) {
 async function openaiCall(prompt, model = OPENAI_API_MODEL, maxTokens = 100, temperature = OPENAI_TEMPERATURE) {
   while (true) {
     try {
-      if (model.startsWith('llama')) {
-        // Spawn a subprocess to run llama.cpp
-        // TODO: Implement llama support
-        throw new Error('Llama support not implemented yet');
-      } else if (!model.startsWith('gpt-')) {
-        // Use completion API
-        const response = await openai.post('/completions', {
+      if (model.startsWith("llama")) {
+        const tempfile = path.resolve(__dirname, "tempfile.txt");
+        fs.writeFileSync(tempfile, prompt);
+        const llama = spawn("./llama", ["-i", tempfile, "-o", tempfile]);
+        let response = "";
+        llama.stdout.on("data", (data) => {
+          response += data.toString();
+        });
+        await new Promise((resolve, reject) => {
+          llama.on("close", (code) => {
+            if (code === 0) {
+              fs.unlinkSync(tempfile);
+              resolve();
+            } else {
+              reject(new Error("Llama subprocess failed"));
+            }
+          });
+        });
+        return response.trim();
+      } else {
+        const response = await openai.post("/completions", {
           engine: model,
           prompt,
           max_tokens: maxTokens,
@@ -86,22 +97,10 @@ async function openaiCall(prompt, model = OPENAI_API_MODEL, maxTokens = 100, tem
           presence_penalty: 0,
         });
         return response.data.choices[0].text.trim();
-      } else {
-        // Use chat completion API
-        const messages = [{ role: 'system', content: prompt }];
-        const response = await openai.post('/engines/davinci-codex/completions', {
-          prompt: '',
-          max_tokens: maxTokens,
-          temperature,
-          n: 1,
-          stop: null,
-          messages,
-        });
-        return response.data.choices[0].text.trim();
       }
     } catch (error) {
       if (error.response && error.response.status === 429) {
-        console.log(chalk.yellow('The OpenAI API rate limit has been exceeded. Waiting 10 seconds and trying again.'));
+        console.log(chalk.yellow("The OpenAI API rate limit has been exceeded. Waiting 10 seconds and trying again."));
         await new Promise((resolve) => setTimeout(resolve, 10000));
       } else {
         throw error;
@@ -112,35 +111,37 @@ async function openaiCall(prompt, model = OPENAI_API_MODEL, maxTokens = 100, tem
 
 async function taskCreationAgent(objective, result, taskDescription, taskList) {
   const prompt = `You are a task creation AI that uses the result of an execution agent to create new tasks with the following objective: ${objective}, The last completed task has the result: ${result}. This result was based on this task description: ${taskDescription}. These are incomplete tasks: ${taskList.join(
-    ', '
+    ", "
   )}. Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks. Return the tasks as an array.`;
-  const response = await openaiCall(prompt, 'davinci-codex', 200); const tasks = response.split(',').map((task) => task.trim()); return tasks.filter((task) => !taskList.includes(task));
+  const response = await openaiCall(prompt, "davinci-codex", 200);
+  const tasks = response.split(",").map((task) => task.trim());
+  return tasks.filter((task) => !taskList.includes(task));
 }
 
-async function executeTask(task) {	
-    const response = await openaiCall(task, OPENAI_API_MODEL);
-    const embedding = await getAdaEmbedding(response);
-    await pinecone.insert(YOUR_TABLE_NAME, [embedding], [task]);
-    return response;
+async function executeTask(task) {
+  const response = await openaiCall(task, OPENAI_API_MODEL);
+  const embedding = await getAdaEmbedding(response);
+  await pinecone.insert(YOUR_TABLE_NAME, [embedding], [task]);
+  return response;
 }
 
 async function getNextTask() {
-    const embedding = await getAdaEmbedding(INITIAL_TASK);
-    const { data } = await pinecone.search(YOUR_TABLE_NAME, [embedding], { k: 1 });
-    return data.hits[0].ids[0];
+  const embedding = await getAdaEmbedding(INITIAL_TASK);
+  const { data } = await pinecone.search(YOUR_TABLE_NAME, [embedding], { k: 1 });
+  return data.hits[0].ids[0];
 }
 
 async function main() {
-    let currentTask = INITIAL_TASK;
-    while (true) {
-    console.log(chalk.blue.bold(\nCurrent task: ${currentTask}));
+  let currentTask = INITIAL_TASK;
+  while (true) {
+    console.log(chalk.blue.bold(`\nCurrent task: ${currentTask}`));
     const result = await executeTask(currentTask);
-    console.log(chalk.green.bold(Result: ${result}));
+    console.log(chalk.green.bold(`Result: ${result}`));
     const tasks = await taskCreationAgent(OBJECTIVE, result, currentTask, taskList);
-    console.log(chalk.yellow.bold(New tasks: ${tasks.join(', ')}));
+    console.log(chalk.yellow.bold(`New tasks: ${tasks.join(", ")}`));
     tasks.forEach((task) => addTask(task));
     currentTask = await getNextTask();
-    }
+  }
 }
 
 main().catch((error) => console.error(error));
