b8155c796360499c8fdf65011704646ce1ba7f8a
– sanz <sanzbernward@gmail.com>
2023-04-20 18:12:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const chalk = require("chalk");
+const say = require('say');
 const { setupPinecone } = require("./small-agi/pineconeHelper");
 const { addTask, taskList } = require("./small-agi/taskHelper");
 const { getAdaEmbedding } = require("./small-agi/openaiHelper");
@@ -10,6 +11,17 @@ const { getNextTask } = require("./small-agi/taskSelector");
 
 setupPinecone();
 
+async function speak(text) {
+  if (process.env.TTS_ENABLED === 'true') {
+    return new Promise((resolve, reject) => {
+      say.speak(text, undefined, undefined, (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+  }
+}
+
 async function main() {
   let currentTask = process.env.INITIAL_TASK || process.env.FIRST_TASK;
 
@@ -17,6 +29,9 @@ async function main() {
     console.log(chalk.blue.bold(`\nCurrent task: ${currentTask}`));
     const result = await executeTask(currentTask, openaiCall, getAdaEmbedding);
     console.log(chalk.green.bold(`Result: ${result}`));
+    
+    await speak(result);
+    
     const tasks = await taskCreationAgent(result, currentTask, taskList, openaiCall);
     console.log(chalk.yellow.bold(`New tasks: ${tasks.join(", ")}`));
     tasks.forEach((task) => addTask(task));
